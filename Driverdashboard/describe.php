635aee46b49cde37585bfbf24a8e5a413c9aeab7
Kabil <kulamkabil07@gmail.com>
2026-06-19 11:15:22 +0530

    fetched backend

--- Driverdashboard/describe.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "db.php";
-$result = $conn->query("DESCRIBE rides;");
-while($row = $result->fetch_assoc()) { print_r($row); }
-$result2 = $conn->query("DESCRIBE users;");
-while($row = $result2->fetch_assoc()) { print_r($row); }
-?>
--- Driverdashboard/show_dbs.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "");
-$res = $conn->query("SHOW DATABASES");
-while($row = $res->fetch_array()){
-    echo $row[0] . "\n";
-}
-?>
